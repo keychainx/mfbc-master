4a4edf9ebc2c8b99ca31d430bfd43e987dcd2b47
mobb <39730701+mobb111@users.noreply.github.com>
2019-02-10 20:31:07 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# mfbc-master
-Multi-Function-Bitcoin-Cracker
+# Made by Ano.mobb
+```
+Work's on windows and linux.
+
+Python3+
+```
+
+# Multi Function Bitcoin Cracker / MFBC
+```
+offline dictionary attack on btc wallets
+
+Donate to the author of this program: 1GmQaG9R5NPs3ZzR6XPMD9jZk17F9MuoWn
+```
+
+# Usage:
+
+```
+$ git https://github.com/mobb111/mfbc-master.git
+
+$ cd mfbc-master
+
+$ pyton mfbc.py
+
+```
+
+# Menu
+
+```
+Select from function 1 to 4, 0 = Exit
+
+1) os.urandom generator 
+2) secrets.token_bytes generator
+3) dict. attack random word gen. (no space)
+4) dict. attack random word gen. (with space)
+0) exit
+```
+
+# How it works
+
+```
+Private key is generated from bytes or passphrases. Then create address and search in txt file address with balance, if it's succsesfull it will be saved in WIN.txt.
+
+Added is BIP0039.txt file for dictionary attack, u can use what ever u want for dictionary attack.
+```
+
+# Proof of Concept
+```
+Although this project can be used maliciously, it is simply an exploration into the Bitcoin protocol and advanced encryption and hashing techniques using Python.
+```
+
+
+
+# If addres found will be printed in txt file:
+```
+address: 17wd8bTwMPYfbo6YwmTFU4xH4hDkd13ATZ
+private key: 0651863bf5d902262b17c4621ec340544ff016752543d99a92d7d22872d8a455
+WIF private key: 5Hs4yg5HYcAmS97UPgmj54V6Lvmv1yKf4yGWs4qcwkWjB5FFnaz
+public key: 040577b1aa3256fed819174b1e6557d4b269f42e383df393b1e8a4c6b9d117994bc476304738fb948cec05a5d17d0fe6f996e7ec0b4a8e2853e41d6657f3f41e33
+
+
+
+address: 125Rs7DdFpNdZgxtPvsQXiM3MDsCEt6F49
+private key: 177c1fdf54c4cb646e3425b7756f5c517c740bdc997b59c968e0a92a0b425f54
+WIF private key: 5HzdUFvvYhiKw97xnZtv5eYWuHKQZ1GMWVChuzkxwuQvrzacPg5
+public key: 04040e4311b932c26f2172e112d1e37bca43428ea6951e3269513ecff6dda67dd8b39666ef34e1dfd82cfdc689d6099905ed9f291f4b4bdf921e81d1b657eee1e6
+```
+
+
+
+```
+Donate to the author of this program: 1GmQaG9R5NPs3ZzR6XPMD9jZk17F9MuoWn
+```
+
+#
+#
+
---
+++ mfbc.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Made by Ano Mobb
+# MultiFunction Bitcoin Cracker/ mfbc
+# 
+
+try:
+    import hashlib, random, os, base64, binascii, ecdsa, base58, secrets, time
+except ImportError:
+    import subprocess
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'base58==1.0.0'])
+    subprocess.check_call(["python", '-m', 'pip', 'install', 'ecdsa==0.13'])
+
+
+
+
+
+
+def print_menu():
+    print ("""
+    
+    
+               ____________________________________________________
+               |                                                  |
+               |   ___________________________________________    |
+               |   |                                         |    |
+               |   |  C:\>_MultiFunction Bitcoin Cracker     |    |
+               |   |  C:\>_                                  |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |  C:\>_                                  |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |                                         |    |
+               |   |   C:\>_Author: Ano.Mobb                 |    |
+               |   |_________________________________________|    |
+               |                                                  |
+                \_________________________________________________/
+                      \___________________________________/
+                    ___________________________________________
+                 _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+              _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+           _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+        _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+     _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_
+    :-------------------------------------------------------------------------:
+    `---._.-------------------------------------------------------------._.---'
+                                  -Ano.Mobb-
+                 Donate: 1GmQaG9R5NPs3ZzR6XPMD9jZk17F9MuoWn
+    """)
+    print ("1) os.urandom generator           " + "\n" +
+           "2) secrets.token_bytes generator  " + "\n" +
+           "3) dict. attack random word gen. (no space)  " + "\n" +
+           "4) dict. attack random word gen. (with space) " + "\n" +
+           "0) Exit " + "\n")
+
+
+
+loop = True
+
+while loop:
+    print_menu()
+    menu = int(input("Choose your number: "))
+    if menu == 1:
+        print ("Menu 1 has been selected")
+        print ("0) Exit")
+        select_byts = int(input("Enter your number of bytes: "))
+        if select_byts == 0:
+            break
+        while True:
+            secret_exponent = os.urandom(select_byts)
+            b64 = base64.b64encode(secret_exponent)
+            pvt = hashlib.sha256(b64).hexdigest()
+            privatekey = binascii.unhexlify(pvt)
+            s = ecdsa.SigningKey.from_string(privatekey, curve = ecdsa.SECP256k1)
+            publickey = '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+            extended_key = "80"+pvt
+            first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+            second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+            final_key = extended_key+second_sha256[:8]
+            WIF = base58.b58encode(binascii.unhexlify(final_key))
+            alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+            c = '0'; byte = '00'; zero = 0
+            var = hashlib.new('ripemd160')
+            var.update(hashlib.sha256(binascii.unhexlify(publickey.encode())).digest())
+            a = (byte + var.hexdigest())
+            doublehash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(a.encode())).digest()).hexdigest()
+            address = a + doublehash[0:8]
+            for char in address:
+                if (char != c):
+                    break
+                zero += 1
+            zero = zero // 2
+            n = int(address, 16)
+            output = []
+            while (n > 0):
+                n, remainder = divmod (n, 58)
+                output.append(alphabet[remainder])
+            count = 0
+            while (count < zero):
+                output.append(alphabet[0])
+                count += 1
+            addr = ''.join(output[::-1])
+            print("base64: " + "  " + str(b64) + "\n" + "Privatekey" + " " + str(pvt)+"\n" + "Salted privatekey: " + str(sal) + "\n" + "Address: " + " " + str(addr) +"\n" + "WIF: " + " " + str(WIF)+"\n")
+            with open("addr-data.txt", "r") as m:
+                add = m.read().split()
+                for ad in add:
+                    continue
+                if addr in add:
+                    data = open("Win","a")
+                    data.write("found " + str(secret_exponent) + "\n" + str(b64) + "\n" + str(pvt)+"\n" +str(addr)+"\n"+str(WIF)+"\n")
+                    data.close()
+                elif addr not in add:
+                    print("No luck!!!"+"\n")
+
+
+    elif menu == 2:
+        print ("Menu 2 has been selected")
+        print ("0) Exit")
+        select_byts = int(input("Enter your number of bytes: "))
+        if select_byts == 0:
+            break
+        while True:
+            secret_exponent = secrets.token_bytes(select_byts)
+            b64 = base64.b64encode(secret_exponent)
+            pvt = hashlib.sha256(b64).hexdigest()
+            privatekey = binascii.unhexlify(pvt)
+            s = ecdsa.SigningKey.from_string(privatekey, curve = ecdsa.SECP256k1)
+            publickey = '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+            extended_key = "80"+pvt
+            first_sha256 = hashlib.sha256(binascii.unhexlify(extended_key)).hexdigest()
+            second_sha256 = hashlib.sha256(binascii.unhexlify(first_sha256)).hexdigest()
+            final_key = extended_key+second_sha256[:8]
+            WIF = base58.b58encode(binascii.unhexlify(final_key))
+            alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+            c = '0'; byte = '00'; zero = 0
+            var = hashlib.new('ripemd160')
+            var.update(hashlib.sha256(binascii.unhexlify(publickey.encode())).digest())
+            a = (byte + var.hexdigest())
+            doublehash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(a.encode())).digest()).hexdigest()
+            address = a + doublehash[0:8]
+            for char in address:
+                if (char != c):
+                    break
+                zero += 1
+            zero = zero // 2
+            n = int(address, 16)
+            output = []
+            while (n > 0):
+                n, remainder = divmod (n, 58)
+                output.append(alphabet[remainder])
+            count = 0
+            while (count < zero):
+                output.append(alphabet[0])
+                count += 1
+            addr = ''.join(output[::-1])
+            print(str(secret_exponent) + "\n" + str(b64) + "\n" + str(pvt)+"\n" +str(addr)+"\n"+str(WIF)+"\n")
+            with open("addr-data.txt", "r") as m:
+                add = m.read().split()
+                for ad in add:
+                    continue
+                if addr in add:
+                    data = open("Win","a")
+                    data.write("found " + str(secret_exponent) + "\n" + str(b64) + "\n" + str(pvt)+"\n" +str(addr)+"\n"+str(WIF)+"\n")
+                    data.close()
+                elif addr not in add:
+                    print("No luck!!!"+"\n")
+
+
+
+        
+            
+     
+    elif menu == 3:
+        print ("Menu 3 has been selected"+ "\n")
+        fobj = input("Enter name or path of txt file: ")
+        sa = input("Enter number of passphrases: ")
+        file = open(fobj, "r")
+        dict = {}
+        while True:
+            with file as f:
+                words = f.read().split()
+                for word in words:
+                    sent = [random.choice(words)
+                            for word in range(int(sa))]
+                    sect = ''.join(sent)
+                    secret_exponent1 = bytes(str(sect).encode("utf-8"))
+                    b6 = base64.b64encode(secret_exponent1)
+                    pvt1 = hashlib.sha256(b6).hexdigest()
+                    privatekey1 = binascii.unhexlify(pvt1)
+                    s = ecdsa.SigningKey.from_string(privatekey1, curve = ecdsa.SECP256k1)
+                    publickey1 = '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+                    extended_key1 = "80"+pvt1
+                    first_sha2561 = hashlib.sha256(binascii.unhexlify(extended_key1)).hexdigest()
+                    second_sha2561 = hashlib.sha256(binascii.unhexlify(first_sha2561)).hexdigest()
+                    final_key1 = extended_key1+second_sha2561[:8]
+                    WIF1 = base58.b58encode(binascii.unhexlify(final_key1))
+                    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+                    c = '0'; byte = '00'; zero = 0
+                    var = hashlib.new('ripemd160')
+                    var.update(hashlib.sha256(binascii.unhexlify(publickey1.encode())).digest())
+                    a = (byte + var.hexdigest())
+                    doublehash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(a.encode())).digest()).hexdigest()
+                    address = a + doublehash[0:8]
+                    for char in address:
+                        if (char != c):
+                            break
+                        zero += 1
+                    zero = zero // 2
+                    n = int(address, 16)
+                    output = []
+                    while (n > 0):
+                        n, remainder = divmod (n, 58)
+                        output.append(alphabet[remainder])
+                    count = 0
+                    while (count < zero):
+                        output.append(alphabet[0])
+                        count += 1
+                    addr1 = ''.join(output[::-1])
+                    print(str(secret_exponent1) + "\n" + str(b6) + "\n" + str(pvt1)+"\n" +str(addr1)+"\n"+str(WIF1)+"\n")
+                    with open("addr-data.txt", "r") as m:
+                        add = m.read().split()
+                        for ad in add:
+                            continue
+                        if addr1 in add:
+                            data = open("Win","a")
+                            data.write("found " + str(secret_exponent1) + "\n" + str(b6) + "\n" + str(pvt1)+"\n" +str(addr1)+"\n"+str(WIF1)+"\n")
+                            data.close()
+                        elif addr1 not in add:
+                            print("No luck!!!"+"\n")
+
+
+
+    elif menu == 4:
+        print ("Menu 4 has been selected"+ "\n")
+        fobj = input("Enter name or path of txt file: ")
+        sa = input("Enter number of passphrases: ")
+        file = open(fobj, "r")
+        dict = {}
+        while True:
+            with file as f:
+                words = f.read().split()
+                f.close()
+                for word in words:
+                    sent = [random.choice(words)
+                            for word in range(int(sa))]
+                    sect = ' '.join(sent)
+                    secret_exponent1 = bytes(str(sect).encode("utf-8"))
+                    b6 = base64.b64encode(secret_exponent1)
+                    pvt1 = hashlib.sha256(b6).hexdigest()
+                    privatekey1 = binascii.unhexlify(pvt1)
+                    s = ecdsa.SigningKey.from_string(privatekey1, curve = ecdsa.SECP256k1)
+                    publickey1 = '04' + binascii.hexlify(s.verifying_key.to_string()).decode('utf-8')
+                    extended_key1 = "80"+pvt1
+                    first_sha2561 = hashlib.sha256(binascii.unhexlify(extended_key1)).hexdigest()
+                    second_sha2561 = hashlib.sha256(binascii.unhexlify(first_sha2561)).hexdigest()
+                    final_key1 = extended_key1+second_sha2561[:8]
+                    WIF1 = base58.b58encode(binascii.unhexlify(final_key1))
+                    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+                    c = '0'; byte = '00'; zero = 0
+                    var = hashlib.new('ripemd160')
+                    var.update(hashlib.sha256(binascii.unhexlify(publickey1.encode())).digest())
+                    a = (byte + var.hexdigest())
+                    doublehash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(a.encode())).digest()).hexdigest()
+                    address = a + doublehash[0:8]
+                    for char in address:
+                        if (char != c):
+                            break
+                        zero += 1
+                    zero = zero // 2
+                    n = int(address, 16)
+                    output = []
+                    while (n > 0):
+                        n, remainder = divmod (n, 58)
+                        output.append(alphabet[remainder])
+                    count = 0
+                    while (count < zero):
+                        output.append(alphabet[0])
+                        count += 1
+                    addr1 = ''.join(output[::-1])
+                    print(str(secret_exponent1) + "\n" + str(b6) + "\n" + str(pvt1)+"\n" +str(addr1)+"\n"+str(WIF1)+"\n")
+                    with open("addr-data.txt", "r") as m:
+                        add = m.read().split()
+                        for ad in add:
+                            continue
+                        if addr1 in add:
+                            data = open("Win","a")
+                            data.write("found " + str(secret_exponent1) + "\n" + str(b6) + "\n" + str(pvt1)+"\n" +str(addr1)+"\n"+str(WIF1)+"\n")
+                            data.close()
+                        elif addr1 not in add:
+                            print("No luck!!!"+"\n")
+
+                    
+
+    elif menu == 0:
+        break
+        loop=False
+    else:
+        # Any integer inputs other than values 1-5 we print an error message
+        key = input("Wrong option selection. Enter any key to try again..")
+
+
+
